8a7d13386f5c340f2494464648c4299e077e78eb
yamini1111 <66066984+yamini1111@users.noreply.github.com>
2022-07-10 06:50:30 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,4 +1,4 @@
-<?php
+//<?php
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
@@ -8,7 +8,7 @@
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'yamini.karri1@gmail.com';
-
+/*
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
@@ -33,9 +33,10 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
+/*  $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
 ?>
+*/
